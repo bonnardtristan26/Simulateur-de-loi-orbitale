53b3f6e4c5ef9d1b360a78545411b700997d9d95
Elric <elric.evrard@stfelixlasalle.fr>
2026-04-28 08:52:41 +0200

    skibid

---
+++ système_solaire/systeme_solaire.py
@@ -0,0 +1,6 @@
+def system(x, *p):
+    pass
+
+system_solaire = ["planete", "masse", "distance_soleil","ForceGravitationnelle"]
+input(system(*system_solaire)).split
+print(system(*system_solaire))
